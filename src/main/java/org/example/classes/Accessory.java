578c2de462cf64ecbc4d33ffa94bc0cfd56589d2
LLumisS <vasa16671@gmail.com>
2023-11-28 22:25:27 +0200

    fix: price check for Accessory

--- src/main/java/org/example/classes/Accessory.java
+++ src/main/java/org/example/classes/Accessory.java
@@ -1,15 +1,25 @@
 package org.example.classes;
 
+import java.util.InputMismatchException;
+
 public class Accessory {
     private final String name;
     private double price;
 
     public Accessory (String name, double price) {
+        if (price < 0) {
+            throw new InputMismatchException("Cannot set negative price");
+        }
+
         this.name = name;
         this.price = price;
     }
 
     public void setPrice(double price) {
+        if (price < 0) {
+            throw new InputMismatchException("Cannot set negative price");
+        }
+
         this.price = price;
     }
 

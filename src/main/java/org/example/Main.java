82121655bdba13ee2af26fa125d0f0f1770fd698
LLumisS <vasa16671@gmail.com>
2023-11-28 22:25:47 +0200

    feat: demonstration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.classes.*;
+import org.example.classes.flowers.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rose rose = new Rose(7, 30);
+        Iris iris = new Iris(5, 25.5);
+        Tulip tulip = new Tulip(10, 35, 23.99);
+        Flower[] flowers = new Flower[]{ rose, iris, tulip };
+
+        Accessory ribbon = new Accessory("Ribbon", 4.99);
+        Accessory[] accessories = new Accessory[]{ ribbon };
+
+        Bouquet bouquet = new Bouquet(flowers, accessories);
+
+        System.out.println(bouquet.getBouquet() + "\n");
+
+        bouquet.sortFlowersByFreshness();
+        System.out.println(bouquet.getBouquet() + "\n");
+
+        System.out.println(bouquet.getFlowersByLength(25, 30));
     }
 }

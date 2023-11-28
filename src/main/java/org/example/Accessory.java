bfc29adf361418959fd423f74a7721508e5bd5b2
LLumisS <vasa16671@gmail.com>
2023-11-28 20:53:32 +0200

    feat: accessory class

---
+++ src/main/java/org/example/Accessory.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Accessory {
+    private final String name;
+
+    public Accessory (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
